986369b2f6b3a1ddc6ef8b225be4b04c189e0d00
Caio <caiusito@gmail.com>
2022-11-11 23:45:13 -0300

    requisito 2

--- tests/unit/models/producsModel.test.js
+++ tests/unit/models/producsModel.test.js
@@ -4,7 +4,7 @@ const connection = require('../../../src/models/connection');
 const { productsModel } = require('../../../src/models');
 const {products, product} = require('./mocks/producsModels.mock')
 
-describe('Testa a camada model de Producs', function () {
+describe('Testa a camada model de Products', function () {
   afterEach(sinon.restore);
 
   it('Valida se o endpoint criado devolve', async function () {
@@ -13,4 +13,9 @@ describe('Testa a camada model de Producs', function () {
     expect(queryResult).to.be.deep.equal(products);
   });
 
-})
+  it('Valida se é possível listar apenas um produto pelo id', async function () {
+    sinon.stub(connection, 'execute').resolves([[product]]);
+    const queryResult = await productsModel.getById(3);
+    expect(queryResult).to.be.deep.equal(product);
+  });
+});
