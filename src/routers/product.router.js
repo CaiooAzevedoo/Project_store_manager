f79a1f36e049e53cec2e132711b523112a84ca71
Caio <caiusito@gmail.com>
2022-11-12 17:57:57 -0300

    Requisito 4

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const validateProducts = require('../middlewares/validateProducts');
 
 const router = express.Router();
 
 router.get('/', productsController.productsList);
 router.get('/:id', productsController.getProduct);
-router.post('/', productsController.postProduct);
+router.post('/', validateProducts.fildNameValidate, productsController.postProduct);
 
 module.exports = router;
--- src/utils/errorMap.js
+++ src/utils/errorMap.js
@@ -1,4 +1,5 @@
 const errorMap = {
+  NAME_IS_REQUIRED: 400,
   PRODUCT_NOT_FOUND: 404,
   INVALID_VALUE: 422,
 };
